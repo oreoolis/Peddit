673ac1d4ece8a843abf0f2773bbb5e8437ca1853
RayleighCN <116514136+RayleighCN@users.noreply.github.com>
2025-10-07 15:17:45 +0800

    Add Map page and link in Navbar

---
+++ src/views/MapView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// TODO: link Map here
+import Map from '../components/Map.vue';
+</script>
+
+<template>
+    <main>
+        <h2>Map</h2>
+        <Map />
+        <!-- <Map /> -->
+    </main>
+</template>
