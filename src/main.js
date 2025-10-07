aeec00b8a862bf3e7eeced931255dd7c2dd9b686
jerememetan <129954878+jerememetan@users.noreply.github.com>
2025-10-07 13:54:34 +0800

    Assests, Sprites, Main CSS Styling
    Added a bunch of assets for use
    -Updated base CSS to override some of bootstrap's code
    -Added font links for custom fonts
    - Updated NavBar
    -Updated PetProfileCard
    -Updated HomeView

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/main.css'
+import './assets/base.css'
+import "bootstrap"
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+
 
 
 const app = createApp(App);
