03d8bed715a0c6e4180c91b1f95ab94cf2fb03e9
jerememetan <129954878+jerememetan@users.noreply.github.com>
2025-10-08 16:21:08 +0800

    Quick Change
    Fixed Commitment Issues
    Updated SocialView Base Template

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,8 +3,6 @@
     </script>
 
     <template>
-        <div >
-            <nav class="navbar fixed-bottom bg-white headingFont" >
         <div>
             <nav class="navbar fixed-bottom bg-white headingFont">
                 <div class="container-fluid">
@@ -33,36 +31,8 @@
                     </ul>
                 </div>
             </nav>
-                <nav class="navbar navbar-expand-sm bg-white headingFont">
-                <div class="container-fluid" >
-                    <a class="navbar-brand primary brandFont" href="/home">Peddit</a>
-                    <ul class="navbar-nav w-100 d-flex justify-content-around flex-row text-center">
-                        <li class="nav-item">
-                            <img src="../assets/Sprite/HomeIcons/Paw.png" alt="">
-                            <RouterLink class="nav-link active black" to="/pet">Pet</RouterLink>
-                        </li>
-                        <li class="nav-item">
-                            <img src="../assets/Sprite/HomeIcons/Home.png" alt="">
-                            <RouterLink class="nav-link active black" to="/home">Home</RouterLink>
-                        </li>
-                        <li class="nav-item">
-                            <img src="../assets/Sprite/HomeIcons/Social.png" alt="">
-                            <RouterLink class="nav-link active primary" to="/social">Social</RouterLink>
-                        </li>
-                        <li class="nav-item">
-                            <img src="../assets/Sprite/HomeIcons/Burger.png" alt="">
-                            <RouterLink class="nav-link active primary" to="/meal">Meal</RouterLink>
-                        </li>
-                        <li class="nav-item">
-                            <!-- INSERT IMAGE HERE -->
-                             <h4>Img here</h4>
-                            <RouterLink class="nav-link active primary" to="/profile">Profile</RouterLink>
-                        </li>
-
-                    </ul>
-                </div>
-            </nav>
-                <nav class="navbar navbar-expand-sm bg-white headingFont">
+            
+            <nav class="navbar navbar-expand-sm bg-white headingFont">
                 <div class="container-fluid" >
                     <a class="navbar-brand primary brandFont" href="/home">Peddit</a>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
--- src/views/SocialView.vue
+++ src/views/SocialView.vue
@@ -12,26 +12,29 @@
             <input type="text" class="form-control rounded-end-pill " placeholder="Search" aria-label="Search" aria-describedby="visible-addon">
             <input type="text" class="form-control d-none " placeholder="Hidden input" aria-label="Hidden input" aria-describedby="visible-addon">
         </div>
-        <div class="text-center headingFont fw-bold ">Profiles</div>
+        <div class="text-center headingFont fw-bold fs-1">Profiles</div>
         <div class="w-75 mx-auto" id="ProfileList">
             <div class="d-flex align-items-center my-2">
                 <img class="rounded-circle float-start" width="65" src="../assets/person.jpg" alt="">
                 <span class="bodyFont px-3">@bernardcks</span>
             </div>
-            <div class="d-flex align-items-center my-2">
-                <img class="rounded-circle float-start" width="65" src="../assets/person.jpg" alt="">
-                <span class="bodyFont px-3">@bernardcks</span>
-            </div>
-            <div class="d-flex align-items-center my-2">
-                <img class="rounded-circle float-start" width="65" src="../assets/person.jpg" alt="">
-                <span class="bodyFont px-3">@bernardcks</span>
-            </div>
-            <div class="d-flex align-items-center my-2">
-                <img class="rounded-circle float-start" width="65" src="../assets/person.jpg" alt="">
-                <span class="bodyFont px-3">@bernardcks</span>
+        </div>
+        <div class="text-center headingFont fw-bold fs-1">Posts</div>
+
+        <div class="w-75 mx-auto" id="PostList">
+            <div class="d-flex align-items-start my-2 rounded  card">
+                <div class="card-body">
+                    <h2 class="card-title">Best Meal for a Puppy</h2>
+                    <div class="d-flex align-items-center">
+                    <img class="rounded-circle float-start pe-2" width="45" src="../assets/person.jpg" alt="">
+                    <h6 class="card-subtitle mb-2 text-body-secondary">@bernardcks</h6>
+                    </div>
+
+
+                </div>
+
             </div>
         </div>
-
 
     </main>
 </template>
