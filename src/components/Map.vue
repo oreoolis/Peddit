95818bd23fecb8b6491dd7cb00151f149e9592dc
RayleighCN <116514136+RayleighCN@users.noreply.github.com>
2025-10-14 16:37:17 +0800

    update to map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -33,6 +33,18 @@ const places = ref([
 onMounted(() => {
   // TODO: Initialize Google Maps here
   // Example: new google.maps.Map(document.getElementById("map"), { ... })
+  const script = document.createElement('script')
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places`
+  script.async = true
+  script.defer = true
+  document.head.appendChild(script)
+
+  script.onload = () => {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 1.3521, lng: 103.8198 }, // Singapore center
+      zoom: 13,
+    })
+  }
 })
 </script>
 
