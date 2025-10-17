7705585fa31470621363e2de81a74b4f8d94a2b8
Dwayne Otero <daa.otero.2024@computing.smu.edu.sg>
2025-10-17 17:23:04 +0800

    parse photoUrl

--- src/components/PetViewComponents/PetCard.vue
+++ src/components/PetViewComponents/PetCard.vue
@@ -28,7 +28,7 @@ const openPetInfo = () => {
                         </svg>
                     </div>
                     <div v-if="gender == 'female'">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="pink"
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="red"
                             class="bi bi-gender-female" viewBox="0 0 16 16">
                             <path fill-rule="evenodd"
                                 d="M8 1a4 4 0 1 0 0 8 4 4 0 0 0 0-8M3 5a5 5 0 1 1 5.5 4.975V12h2a.5.5 0 0 1 0 1h-2v2.5a.5.5 0 0 1-1 0V13h-2a.5.5 0 0 1 0-1h2V9.975A5 5 0 0 1 3 5" />
@@ -71,6 +71,7 @@ const openPetInfo = () => {
         :birthday="birthday"
         :weight="weight"
         :allergies="allergies"
+        :photo_url="photo_url"
      />
 
 </template>
