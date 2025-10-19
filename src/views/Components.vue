03479bf75c630bc8c12d08a58b759be17f433909
Bernardcks <bernardchua.2024@computing.smu.edu.sg>
2025-10-19 13:54:55 +0800

    No conflict pls

---
+++ src/views/Components.vue
@@ -0,0 +1,55 @@
+<script setup>
+import Button from "../components/atoms/button.vue"
+import searchBar from "@/components/atoms/searchBar.vue";
+import PetProfileCard from "@/components/molecules/PetProfileCard.vue";
+import PetCard from "@/components/molecules/PetCard.vue";
+
+</script>
+<template>
+    <!-- BUTTONS! -->
+    <!-- You can add custom labels with label="Your Text Here"
+    You can also add outline by adding outline="true"
+    You can change colors by adding color="secondary"  -->
+     <div class="row row-cols-3">
+        <div class="col">
+            <Button class="m-2" label="This is a custom text!">
+            <i class="bi bi-heart-fill mx-1" ></i>
+            </Button>
+            <Button class="m-2" label="This is a custom text!" color="secondary">
+            <i class="bi bi-heart-fill mx-1" ></i>
+            </Button>
+            <Button class="m-2" label="This is a custom text!" color="danger">
+            <i class="bi bi-heart-fill mx-1"></i>
+            </Button>
+        </div>
+
+        <div class="col">
+            <Button class="m-2" label="This is a custom text!">
+            </Button>
+            <Button class="m-2" label="This is a custom text!" color="secondary">
+            </Button>
+            <Button class="m-2" label="This is a custom text!" color="danger">
+            </Button>
+        </div>
+
+        <div class="col">
+            <Button class="m-2" label="This is a custom text!" outline="true">
+            <i class="bi bi-heart-fill mx-1 " ></i>
+            </Button>
+            <Button class="m-2" label="This is a custom text!" color="secondary" outline="true">
+            <i class="bi bi-heart-fill mx-1 " ></i>
+            </Button>
+            <Button class="m-2" label="This is a custom text!" color="danger" outline="true">
+            <i class="bi bi-heart-fill mx-1 "></i>
+            </Button>
+        </div>        
+     </div>
+
+     <!-- SEARCH BAR -->
+<searchBar></searchBar>
+
+     <!-- PET CARD -->
+<PetCard></PetCard>>
+
+
+</template>
