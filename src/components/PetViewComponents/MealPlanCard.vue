b96211045b28caec62429e2db8bfd4c7ea424506
jerememetan <129954878+jerememetan@users.noreply.github.com>
2025-10-23 23:08:29 +0800

    made MealPlanCard Dynamic as well :D

--- src/components/PetViewComponents/MealPlanCard.vue
+++ src/components/PetViewComponents/MealPlanCard.vue
@@ -1,7 +1,26 @@
 <script setup>
 import MealInfoModal from './MealInfoModal.vue';
 import { ref } from 'vue';
+const props = defineProps({
+    Recipe_Name: {
+        default: "Recipe_Name",
+        type: String,
+    },
+    Recipe_Description: {
+        default: "Recipe Description",
+        type: String,
+    },
+    Breed_Type: {
+        default: "Breed_Type",
+        type: String,
+    },
+    Image: {
+        default: "./src/assets/Sprite/Food/Set3-Meat/11.png",
+        type: String,
+    }
 
+    }
+);
 const showMealInfo = ref(false);
 const openMealInfo = () => {
     showMealInfo.value = true;
@@ -10,11 +29,12 @@ const openMealInfo = () => {
 <template>
     <div class="recipeCard card overflow-hidden bg-primary shadow p-3 mb-5 rounded-5" style="width: 18rem; height: 27rem;">
         <!-- to show the first ingredient item -->
-        <img src="../../assets/Sprite/Food/Set3-Meat/11.png" class="card-img-top img-thumbnail" alt="...">
+        <img :src="Image" class="card-img-top img-thumbnail" alt="...">
         <div class="card-body">
             <div class="card-text text-center">
-                <h3 class="headingFont text-light">Recipe</h3>
-                <p class="bodyFont text-light">Breed type</p>
+                <h3 class="headingFont text-light">{{props.Recipe_Name}}</h3>
+                <p class="bodyFont text-light">{{Breed_Type}}</p>
+                <p class="bodyFont text-light">{{Recipe_Description}}</p>
             </div>
             <div class="summary-container container-fluid position-absolute bottom-0 start-0 end-0 px-3 py-3 bg-primary"
                 @click="openMealInfo">
