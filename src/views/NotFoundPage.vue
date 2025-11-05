5f7c22acf121be2d7ace66409a1050ede92c99d5
Bernardcks <bernardchua.2024@computing.smu.edu.sg>
2025-11-05 14:00:00 +0800

    Not found page design

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,13 +1,17 @@
 <script setup>
-
+import AnimatedImage from '@/components/atoms/animated/animatedImage.vue';
 </script>
 
 <template>
-    <div>
-        <h1>404 Page Not Found</h1>
+    <div class="d-flex">
+        <div class="mx-auto d-flex align-content-center">
+            <AnimatedImage src="/src/assets/Sprite/Dog/Idle.png" :frameWidth="48" :frameHeight="48" :frames="4" :fps="4" :width="64" :height="64" />
+            <h1>404 Page Not Found</h1>
+            <AnimatedImage src="/src/assets/Sprite/Cat/Idle.png" :frameWidth="48" :frameHeight="48" :frames="4" :fps="4" :width="64" :height="64" />
+        </div>
     </div>
 </template>
 
 <style scoped>
-    
+
 </style>
